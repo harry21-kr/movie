19dbd6f5c63549fc2a48203218131f20de0ea94d
박원빈 <harry21@kakao.com>
2024-04-23 14:35:10 +0900

    (design) detail dialog style

--- js/utils/renderMoviesDetailDialog.js
+++ js/utils/renderMoviesDetailDialog.js
@@ -1,47 +1,43 @@
 import { TMDB_IMAGE_URL } from "../config/constants/index.js";
 
 export function renderMoviesDetailDialog(data) {
-  const oldTitleTextTag = document.querySelector(
-    ".movie-detail-dialog-title-text"
-  );
-  const oldVoteAverageTag = document.querySelector(
-    ".movie-detail-dialog-vote-average-text"
-  );
-  const oldImgTag = document.querySelector(".movie-detail-img");
-  const oldReleaseDateTag = document.querySelector(
-    ".movie-detail-dialog-release-date-text"
-  );
+  const oldWrapTag = document.querySelector(".movie-detail-dialog-wrap");
 
-  if (oldImgTag && oldTitleTextTag && oldVoteAverageTag && oldReleaseDateTag) {
-    oldImgTag.remove();
-    oldReleaseDateTag.remove();
-    oldTitleTextTag.remove();
-    oldVoteAverageTag.remove();
+  if (oldWrapTag) {
+    oldWrapTag.remove();
   }
 
   const dialogTag = document.getElementById("movie-detail-dialog");
+
+  const wrapTag = document.createElement("div");
+  wrapTag.classList.add("movie-detail-dialog-wrap");
+
+  const textWrapTag = document.createElement("div");
+  textWrapTag.classList.add("movie-detail-dialog-text-wrap");
 
   const titleTag = document.createElement("h4");
   titleTag.classList.add("movie-detail-dialog-title-text");
   titleTag.textContent = data.title;
 
-  const voteAverageTag = document.createElement("p");
-  voteAverageTag.classList.add("movie-detail-dialog-vote-average-text");
-  voteAverageTag.textContent = `평점: ${data.vote_average.toFixed(1)}점`;
+  const descriptionTag = document.createElement("p");
+  descriptionTag.classList.add("movie-detail-dialog-description-text");
+  descriptionTag.textContent = data.overview;
 
   const posterTag = document.createElement("img");
-  posterTag.classList.add("movie-detail-img");
-  posterTag.src = `${TMDB_IMAGE_URL}${data.poster_path}`;
+  posterTag.classList.add("movie-detail-dialog-img");
+  posterTag.src = `https://image.tmdb.org/t/p/original/${data.backdrop_path}`;
 
   const releaseDateTag = document.createElement("p");
   const formatReleaseDate = data.release_date.replace(/-/gi, ".");
   releaseDateTag.classList.add("movie-detail-dialog-release-date-text");
-  releaseDateTag.textContent = `개봉 일자: ${formatReleaseDate}`;
+  releaseDateTag.textContent = formatReleaseDate;
 
-  dialogTag.appendChild(posterTag);
-  dialogTag.appendChild(titleTag);
-  dialogTag.appendChild(voteAverageTag);
-  dialogTag.appendChild(releaseDateTag);
+  textWrapTag.appendChild(titleTag);
+  textWrapTag.appendChild(releaseDateTag);
+  textWrapTag.appendChild(descriptionTag);
+  wrapTag.appendChild(posterTag);
+  wrapTag.appendChild(textWrapTag);
+  dialogTag.appendChild(wrapTag);
 
   dialogTag.addEventListener("click", () => dialogTag.close());
 
