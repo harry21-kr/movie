8d45779efb64b0f38023d4e3d0f755c6f6412bf0
박원빈 <harry21@kakao.com>
2024-04-23 15:59:14 +0900

    (refactor) remove themovie.js

--- js/config/themovie.js
+++
@@ -1,8 +0,0 @@
-export const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDk0MDE0NDY3MDNhZTJiMzRkNzkxZGY1NjQ2ZDcxMyIsInN1YiI6IjY2MjVjOTEyZWI3OWMyMDE2NWQzZmRhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.icYgLQMDyGlyR1sZyjnzEoWMwNRCqN_yzoEC7t-Zcw4",
-  },
-};
